3de39d5f1c05a3c772db21c2bb73b352996e68de
Carson Keller <carsonjkeller@gmail.com>
2020-06-18 17:23:00 -0600

    Edited base routing to include PlantContext, provider, and define context functions to access plant and product APIs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { PlantContext } from './context/PlantContext';
+import { Plants } from './pages/Plants';
 
 function App() {
-  return (
-    <div className="App">
+  const { Provider } = PlantContext;
+  const [plants, setPlants] = useState([]);
+  const [products, setProducts] = useState([]);
 
-    </div>
+  const getPlants = () => {
+    const url = process.env.REACT_APP_PLANTS_URL;
+    fetch(`${url}`)
+      .then(response => response.json())
+      .then(data => setPlants(data))
+      .catch(err => err);
+  };
+
+  const getProducts = () => {
+    const url = process.env.REACT_APP_PRODUCTS_URL;
+    fetch(`${url}`)
+      .then(response => response.json())
+      .then(data => setProducts(data))
+      .catch(err => err);
+  };
+
+  useEffect(() => {getPlants(); getProducts()}, []);
+
+  return (
+    <Provider value={{ plants, getPlants, products, getProducts }}>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Plants} />
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 
