824708fc5e32fc34c28471bfb4e657c92de5897a
Carson Keller <carsonjkeller@gmail.com>
2020-07-03 21:56:51 -0600

    Added temporary context variable to access locally stored plant variable

--- src/context/PlantContext.js
+++ src/context/PlantContext.js
@@ -3,6 +3,8 @@ import { createContext } from 'react';
 export const PlantContext = createContext({
     plants: [],
     getPlants: () => {},
+    // plant: [],
+    // getPlant: () => {},
     products: [],
     getProducts: () => {},
 });
