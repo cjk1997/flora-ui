9260e86c3781421471e8eb3075f0ad4d8c23d367
Carson Keller <carsonjkeller@gmail.com>
2020-07-06 10:53:12 -0600

    Created context function and variable and added them to the provider

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { EditPlant } from './pages/EditPlant';
 function App() {
   const { Provider } = PlantContext;
   const [plants, setPlants] = useState([]);
-  // const [plant, setPlant] = useState([]);
+  const [plant, setPlant] = useState([]);
   const [products, setProducts] = useState([]);
 
   const getPlants = () => {
@@ -18,12 +18,12 @@ function App() {
       .catch(err => err);
   };
 
-  // const getPlant = () => {
-  //   if (localStorage.getItem('plant')) {
-  //     const plant = JSON.parse(localStorage.getItem('plant')).plant;
-  //     setPlant(plant);
-  //   };
-  // };
+  const getPlant = () => {
+    if (localStorage.getItem('plant')) {
+      const plant = JSON.parse(localStorage.getItem('plant')).plant;
+      setPlant(plant);
+    };
+  };
 
   const getProducts = () => {
     const url = process.env.REACT_APP_PRODUCTS_URL;
@@ -48,8 +48,10 @@ function App() {
 
   useEffect(() => {getPlants(); getProducts()}, []);
 
+  useEffect(() => {if (localStorage.getItem('plant')) getPlant()}, [])
+
   return (
-    <Provider value={{ plants, getPlants, products, getProducts }}>
+    <Provider value={{ plants, getPlants, plant, getPlant, products, getProducts }}>
       <Router>
         <Switch>
           <Route exact path='/' component={Plants} />
